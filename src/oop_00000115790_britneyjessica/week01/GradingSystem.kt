d8447669a366080cb82746de89ed752a55322aa8
britney <saslimbritney@gmail.com>
2026-02-06 11:04:44 +0700

    week01: add calculateStatus function

--- src/oop_00000115790_britneyjessica/week01/GradingSystem.kt
+++ src/oop_00000115790_britneyjessica/week01/GradingSystem.kt
@@ -3,7 +3,13 @@ package oop_00000115790_britneyjessica.week01
 fun main () {
     val name: String = "BritBrit"
     val score: Int = 80
+    val grade = when (score) {
+        in 90 .. 100 -> "A"
+        in 80 .. 89 -> "B"
+        in 70 .. 79-> "C"
+        else -> "D"
+    }
 
     println("Nama: " + name + ", Nilai: " + score)
-
+    println("Grade: $grade")
 }
