bedefe3c3f83daecbb7db188b1961c830479f47e
britney <saslimbritney@gmail.com>
2026-02-06 15:23:44 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000115790_britneyjessica/week01/GameStore.kt
+++ src/oop_00000115790_britneyjessica/week01/GameStore.kt
@@ -4,6 +4,14 @@ fun main() {
 
     val gameTitle = "Mario Kart"
     val price = 650000
+
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
+
+    printReceipt(
+        title = gameTitle,
+        finalPrice = finalPrice
+    )
 }
 
 fun calculateDiscount(price: Int): Int =
@@ -13,3 +21,7 @@ fun calculateDiscount(price: Int): Int =
         price * 10 / 100
     }
 
+fun printReceipt(title: String, finalPrice: Int) {
+    println("Game Title: $title")
+    println("Final Price: Rp$finalPrice")
+}
