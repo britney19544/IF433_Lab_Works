a24844e103fc4cc3391798522aa665b4e1805159
britney <saslimbritney@gmail.com>
2026-02-06 15:21:12 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000115790_britneyjessica/week01/GameStore.kt
+++ src/oop_00000115790_britneyjessica/week01/GameStore.kt
@@ -6,3 +6,10 @@ fun main() {
     val price = 650000
 }
 
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) {
+        price * 20 / 100
+    } else {
+        price * 10 / 100
+    }
+
