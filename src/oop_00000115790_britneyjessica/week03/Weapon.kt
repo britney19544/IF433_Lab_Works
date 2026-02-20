1071ce02a6d022fc352e24281b79668892ddd8ea
britney <saslimbritney@gmail.com>
2026-02-20 11:23:29 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_00000115790_britneyjessica/week03/Weapon.kt
@@ -0,0 +1,27 @@
+package oop_00000115790_britneyjessica.week03
+
+class Weapon(val name: String) {
+
+    var damage: Int = 0
+        set(value) {
+            when {
+                value < 0 -> {
+                    println("WARNING: Damage tidak boleh negatif!")
+                }
+                value > 1000 -> {
+                    println("WARNING: Damage terlalu besar! Diset ke 1000.")
+                    field = 1000
+                }
+                else -> {
+                    field = value
+                }
+            }
+        }
+
+    val tier: String
+        get() = when {
+            damage > 800 -> "Legendary"
+            damage > 500 -> "Epic"
+            else -> "Common"
+        }
+}
