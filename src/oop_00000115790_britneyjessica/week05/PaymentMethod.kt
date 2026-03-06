d64ab7f6cff0574129a8d628f5c9f28b84462d6b
britney <saslimbritney@gmail.com>
2026-03-06 14:55:42 +0700

    week05: (task 2) implement payment polymorphism

--- src/oop_00000115790_britneyjessica/week05/PaymentMethod.kt
+++ src/oop_00000115790_britneyjessica/week05/PaymentMethod.kt
@@ -3,3 +3,18 @@ package oop_00000115790_britneyjessica.week05
 abstract class PaymentMethod(val accountName: String) {
     abstract fun processPayment(amount: Double)
 }
+
+fun main() {
+    // Step 4: Instantiation
+    val myEWallet = EWallet("User123", 50000.0)
+    val myCreditCard = CreditCard("User123", 100000.0)
+
+    val paymentList: List<PaymentMethod> = listOf(myEWallet, myCreditCard)
+
+    // Checkpoint 10 logic: Initial loop
+    for (payment in paymentList) {
+        println("--- Memproses pembayaran untuk: ${payment.accountName} ---")
+        payment.processPayment(75000.0)
+
+    }
+}
